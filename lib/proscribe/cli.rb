55269017d2ca84855b7d8b69fcf2c6675b35c3f1
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-25 02:03:37 +0800

    Add proscribe gh-pages.

--- lib/proscribe/cli.rb
+++ lib/proscribe/cli.rb
@@ -41,5 +41,48 @@ class ProScribe::CLI < Shake
     copy_files ProScribe.root('data/rack'), dir
   end
   task.description = "Makes a project Rack-compatible"
+
+  # proscribe ghpages rstacruz/proscribe
+  task (:'gh-pages') do
+    project
+    path  = project.root(project.config.output)
+    where = params.first or wrong_usage
+    repo  = "git@github.com:#{where}.git"
+
+    status "Deploying to #{repo}..."
+    status "(WARNING: This will overwrite history. Press ^C to abort.)"
+
+    gitdeploy(repo, "gh-pages")
+  end
+  task.usage = "gh-pages username/repo"
+  task.description = "Deploy to GitHub pages."
+
+  invalid do
+    if task(command)
+      usage = task(command).usage || command
+
+      err "Invalid usage."
+      err "Usage: #{executable} #{usage}"
+      err "See `#{executable} help` for more info."
+    else
+      err "Unknown command: #{command}"
+      err "See `#{executable} help` for a list of commands."
+    end
+  end
+
+  def self.gitdeploy(dest, branch)
+    project
+    path = project.root(project.config.output)
+
+    temppath = File.join(Dir.tmpdir, "proton-#{Time.now.to_i}")
+    copy_files path, temppath
+
+    Dir.chdir(temppath) {
+      system "(git init . && git add . && git commit -m .) > /dev/null"
+      system "git push #{dest} master:#{branch} --force"
+    }
+
+    FileUtils.rm_rf temppath
+  end
+
 end
-  
