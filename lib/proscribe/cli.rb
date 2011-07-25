000877a35920e2e234670fcb7a96c7c90bdbb83b
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-26 03:20:35 +0800

    Remove ghpages support.

--- lib/proscribe/cli.rb
+++ lib/proscribe/cli.rb
@@ -42,21 +42,6 @@ class ProScribe::CLI < Shake
   end
   task.description = "Makes a project Rack-compatible"
 
-  # proscribe ghpages rstacruz/proscribe
-  task (:'gh-pages') do
-    project
-    path   = project.root(project.config.output)
-    where  = params[0] or wrong_usage
-    prefix = params[1] || ''
-    repo   = "git@github.com:#{where}.git"
-
-    puts "Fetching #{repo} (gh-pages)"
-
-    gitdeploy(repo, "gh-pages", prefix)
-  end
-  task.usage = "gh-pages username/repo [dirprefix]"
-  task.description = "Deploy to GitHub pages"
-
   invalid do
     if task(command)
       usage = task(command).usage || command
@@ -69,45 +54,4 @@ class ProScribe::CLI < Shake
       err "See `#{executable} help` for a list of commands."
     end
   end
-
-  def self.gitdeploy(dest, branch, prefix='')
-    project
-    path = project.root(project.config.output)
-
-    temppath = File.join(Dir.tmpdir, "proton-#{Time.now.to_i}")
-    FileUtils.rm_rf temppath
-
-    # Clone the gh-pages branch.
-    system "git clone #{dest} -b #{branch} #{temppath}"
-
-    current_branch = Dir.chdir(temppath) { `git symbolic-ref HEAD`.strip.split('/') }
-
-    if current_branch.last == branch
-      puts "Adding files to #{prefix}/..."
-      # Did we get the correct branch? Just add on top of it.
-      copy_files path, File.join(temppath, prefix)
-
-      #system "(git add .; git add -u; git commit -m .) > /dev/null"
-      Dir.chdir(temppath) {
-        system "(git add .; git add -u; git commit -m .)"
-        system "git push origin #{branch}"
-      }
-    else
-      puts "Warning: No #{branch} branch found, starting over."
-
-      # Else, clean it and start over.
-      FileUtils.rm_rf temppath
-      FileUtils.mkdir temppath
-
-      copy_files path, File.join(temppath, prefix)
-
-      Dir.chdir(temppath) {
-        system "(git init . && git add . && git commit -m .) > /dev/null"
-        system "git push #{dest} master:#{branch} --force"
-      }
-    end
-
-    # FileUtils.rm_rf temppath
-  end
-
 end
