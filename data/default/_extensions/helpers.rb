9795b44883f8ca7d9d6bd44db8922b53769c5840
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-26 23:21:44 +0800

    Literate syntax in <pre>.

---
+++ data/default/_extensions/helpers.rb
@@ -0,0 +1,32 @@
+module Proton::Helpers
+  def page_children(page)
+    children = page.children
+    of_type  = lambda { |str| children.select { |p| p.html? && p.meta.page_type == str } }
+
+    children.
+      select { |p| p.html? }.
+      group_by { |p|
+        type = p.meta.page_type
+        type.nil? ? nil : Inflector[type].pluralize.to_sym
+      }
+  end
+end
+
+# Inflector['hello'].pluralize
+class Inflector < String
+  def self.[](str)
+    new str.to_s
+  end
+
+  def pluralize
+    if self[-1] == 's'
+      "#{self}es"
+    else
+      "#{self}s"
+    end
+  end
+
+  def sentencize
+    self.gsub('_', ' ').capitalize
+  end
+end
